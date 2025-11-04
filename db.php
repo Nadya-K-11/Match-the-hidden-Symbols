b38b0517b8b9c1047f71419ac8c8327a0cd8af60
Nadya A. K. <nad_a_k@abv.bg>
2025-11-04 18:53:37 +0200

    Create db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$db   = "match_the_hidden_symbols";
+$user = "root";
+$pass = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Грешка при връзка с базата: " . $e->getMessage());
+}
